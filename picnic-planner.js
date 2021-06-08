eaf4c7edc9736946948c36bebd44574b27142651
HK-RazieL <s.angelkov@abv.bg>
2021-06-08 09:41:57 +0300

    Minor naming fixes

--- picnic-planner.js
+++ picnic-planner.js
@@ -1,8 +1,9 @@
 "use strict"
 
 const https = require("https");
-
-const input = process.argv[2];
+const MIN_TEMP = 10;
+const ACCEPTABLE_WEATHER = ["Heavy Cloud", "Light Cloud", "Clear"];
+const INPUT = process.argv[2];
 
 function getRequestData(url) {
     return new Promise((resolve, reject) => {
@@ -30,47 +31,62 @@ function getWeekendDates() {
 }
 
 function checkWeather(day) {
-    var highestTemp = 0;
-    var bestWeather = 0;
-    var weather = ["Heavy Cloud", "Light Cloud", "Clear"]
+    let highestTemp = 0;
+    let bestWeatherIndex = 0;
     for (const hour of day) {
-        if (hour.the_temp < 10 ||
-            hour.weather_state_name !== "Clear" &&
-            hour.weather_state_name !== "Light Cloud" &&
-            hour.weather_state_name !== "Heavy Cloud") {
+        if (hour.the_temp < MIN_TEMP || !ACCEPTABLE_WEATHER.includes(hour.weather_state_name)) {
             return false;
         }
         if (hour.the_temp > highestTemp) {
             highestTemp = hour.the_temp;
         }
-        if (weather.indexOf(hour.weather_state_name) > weather.indexOf(bestWeather)) {
-            bestWeather = weather.indexOf(hour.weather_state_name);
+        if (ACCEPTABLE_WEATHER.indexOf(hour.weather_state_name) > ACCEPTABLE_WEATHER.indexOf(bestWeatherIndex)) {
+            bestWeatherIndex = ACCEPTABLE_WEATHER.indexOf(hour.weather_state_name);
         }
         highestTemp = hour.the_temp;
-        bestWeather = weather.indexOf(hour.weather_state_name);
+        bestWeatherIndex = ACCEPTABLE_WEATHER.indexOf(hour.weather_state_name);
     }
-    return [highestTemp, bestWeather];
+    return {
+        highestTemp,
+        bestWeather: bestWeatherIndex
+    };
 }
 
 function compareWeather(saturday, sunday) {
     let name;
     let reasons = [];
-    if (saturday[1] === sunday[1]) {
-        name = saturday[0] > sunday[0] ? "Saturday" : "Sunday"
+    if (saturday.bestWeather === sunday.bestWeather) {
+        name = saturday.highestTemp > sunday.highestTemp ? "Saturday" : "Sunday"
         reasons.push("warmer")
-    } else if (saturday[1] > sunday[1]) {
-        name = saturday[1] > sunday[1] ? "Saturday" : "Sunday"
+    } else if (saturday.bestWeather > sunday.bestWeather) {
+        name = saturday.bestWeather > sunday.bestWeather ? "Saturday" : "Sunday"
         reasons.push("better weather")
     }
-    
+
     return {
         name,
         reasons
     }
 }
 
+function printingResults(saturday, sunday) {
+    if (saturday && sunday) {
+        const betterDay = compareWeather(saturday, sunday)
+        console.log(`This weekend looks nice for a picnic, ${betterDay.name} is best because it's ${betterDay.reasons.length > 1 ?
+        betterDay.reasons[0] + "and" + betterDay.reasons[1] : betterDay.reasons[0]}.`);
+    } else if (saturday || sunday) {
+        console.log(`You should have your picnic on ${saturday ? "Saturday" : "Sunday"}`);
+    } else {
+        console.log("The weather isn't looking very good this weekend, maybe stay indoors.");
+    }
+}
+
 (async () => {
-    const locationId = await getRequestData("https://www.metaweather.com/api/location/search/?query=" + input);
+    const locationId = await getRequestData("https://www.metaweather.com/api/location/search/?query=" + INPUT);
+    if (!locationId.length) {
+        console.error(`${INPUT} is not found`);
+        process.exit(1);
+    }
     const weekendDates = getWeekendDates();
     const locationWeatherSaturday = await getRequestData("https://www.metaweather.com/api/location/" +
         locationId[0].woeid + "/" + weekendDates[0] + "/");
@@ -79,14 +95,5 @@ function compareWeather(saturday, sunday) {
 
     const saturdayWeather = checkWeather(locationWeatherSaturday);
     const sundayWeather = checkWeather(locationWeatherSunday);
-
-    if (saturdayWeather && sundayWeather) {
-        const betterDay = compareWeather(saturdayWeather, sundayWeather)
-        console.log(`This weekend looks nice for a picnic, ${betterDay.name} is best because it's ${betterDay.reasons.length > 1 ?
-        betterDay.reasons[0] + "and" + betterDay.reasons[1] : betterDay.reasons[0]}.`);
-    } else if (saturdayWeather || sundayWeather) {
-        console.log(`You should have your picnic on ${saturdayWeather ? "Saturday" : "Sunday"}`);
-    } else {
-        console.log("The weather isn't looking very good this weekend, maybe stay indoors.");
-    }
+    printingResults(saturdayWeather, sundayWeather)
 })()
