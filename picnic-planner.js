63fe2c147280a50158449da998830dfe18023599
HK-RazieL <s.angelkov@abv.bg>
2021-06-08 10:04:29 +0300

    Separated urls into constants

--- picnic-planner.js
+++ picnic-planner.js
@@ -1,9 +1,12 @@
 "use strict"
 
 const https = require("https");
+
+const INPUT = process.argv.slice(2).join("%20");
 const MIN_TEMP = 10;
 const ACCEPTABLE_WEATHER = ["Heavy Cloud", "Light Cloud", "Clear"];
-const INPUT = process.argv.slice(2).join("%20");
+const WOEID_URL = "https://www.metaweather.com/api/location/search/?query=";
+const WEATHER_URL = "https://www.metaweather.com/api/location/";
 
 function getRequestData(url) {
     return new Promise((resolve, reject) => {
@@ -82,16 +85,14 @@ function printingResults(saturday, sunday) {
 }
 
 (async () => {
-    const locationId = await getRequestData("https://www.metaweather.com/api/location/search/?query=" + INPUT);
+    const locationId = await getRequestData(WOEID_URL + INPUT);
     if (!locationId.length) {
         console.error(`${INPUT} is not found`);
         process.exit(1);
     }
     const weekendDates = getWeekendDates();
-    const locationWeatherSaturday = await getRequestData("https://www.metaweather.com/api/location/" +
-        locationId[0].woeid + "/" + weekendDates[0] + "/");
-    const locationWeatherSunday = await getRequestData("https://www.metaweather.com/api/location/" +
-        locationId[0].woeid + "/" + weekendDates[1] + "/");
+    const locationWeatherSaturday = await getRequestData(`${WEATHER_URL}${locationId[0].woeid}/${weekendDates[0]}/`);
+    const locationWeatherSunday = await getRequestData(`${WEATHER_URL}${locationId[0].woeid}/${weekendDates[1]}/`);
 
     const saturdayWeather = checkWeather(locationWeatherSaturday);
     const sundayWeather = checkWeather(locationWeatherSunday);
